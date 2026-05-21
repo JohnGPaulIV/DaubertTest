464abae09a518ee1124114f8deb29bc69b913f80
John Paul <daubert@jgpiv.com>
2026-05-21 03:50:46 +0000

    Starting files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white text-4xl">
+      My Production App
+    </div>
+  );
+}
+
+export default App
